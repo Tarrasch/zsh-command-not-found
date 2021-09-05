9d04162a4837c2e98d7c912879fa4f95156af6bc
Freed-Wu <wuzy01@qq.com>
2021-09-05 14:53:29 +0800

    command-not-found support for homebrew (linuxbrew)

--- command-not-found.plugin.zsh
+++ command-not-found.plugin.zsh
@@ -7,3 +7,13 @@
 # Arch Linux command-not-found support, you must have package pkgfile installed
 # https://wiki.archlinux.org/index.php/Pkgfile#.22Command_not_found.22_hook
 [[ -e /usr/share/doc/pkgfile/command-not-found.zsh ]] && source /usr/share/doc/pkgfile/command-not-found.zsh
+
+# Homebrew (Linuxbrew) command-not-found support
+# https://docs.brew.sh/Manpage#command-not-found-init
+if [[ -z $HOMEBREW_PREFIX ]] && (($+commands[brew])); then
+  eval $(brew shellenv)
+fi
+if [[ -n $HOMEBREW_PREFIX ]]; then
+  HB_CNF_HANDLER="$HOMEBREW_PREFIX/Homebrew/Library/Taps/homebrew/homebrew-command-not-found/handler.sh"
+  [[ -e "$HB_CNF_HANDLER" ]] && source "$HB_CNF_HANDLER"
+fi
